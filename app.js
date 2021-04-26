df8221bb75cb8fd03c19f5f8c551f4b1606a057b
Suelen Almeida <lima.j.diego@gmail.com>
2021-04-26 17:10:20 -0300

    Adicionando o bootstrap e o componente navbar

--- app.js
+++ app.js
@@ -2,21 +2,28 @@
     const express = require('express')
     const handlebars = require('express-handlebars')
     const bodyParser = require('body-parser')
+    
     const app = express()
     const admin = require('./routes/admin')
+    const path = require('path')
+
     //const mongoose = require('mongoose')
 //Configuração
     
-    //body-parser
+    //Body-Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
     
-    //handlebars
+    //Handlebars
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
-    // mongoose
+    // Mongoose
         //Em breve   
     
+    // Public
+        app.use(express.static(path.join(__dirname, 'public')))
+
+
 //Rotas
     app.use('/admin', admin)
     
