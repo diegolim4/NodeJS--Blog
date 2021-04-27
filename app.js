75ada3d202239ebbe4e42c46f27b482325ca0c61
Suelen Almeida <lima.j.diego@gmail.com>
2021-04-27 15:21:37 -0300

    Sessões configuradas para armazenamento de informações temporárias

--- app.js
+++ app.js
@@ -8,9 +8,25 @@
     const path = require('path')
 
     const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
 
 //Configuração
-    
+    //Sessão
+        app.use(session ({
+            secret: 'amonode',   // uma chave para gerar a minha sessão
+            resave: true,
+            saveUninitialized: true 
+        }))
+        app.use(flash())  //chamando o flash
+       
+    //Middleware
+        app.use((req, res, next)=>{
+            res.locals.success_msg = req.flash('sucess_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next() 
+        })
+
     //Body-Parser
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json())
@@ -30,7 +46,8 @@
     // Public
         app.use(express.static(path.join(__dirname, 'public')))
 
-        //Rotas
+       
+ //Rotas
     app.use('/admin', admin)  
 
 //Outros
