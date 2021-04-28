91ef9ff0ba4b720323dd7dfa22045407259eccd7
Suelen Almeida <lima.j.diego@gmail.com>
2021-04-28 09:28:31 -0300

    Corrigido erro de digitação no middleware

--- app.js
+++ app.js
@@ -22,7 +22,7 @@
        
     //Middleware
         app.use((req, res, next)=>{
-            res.locals.success_msg = req.flash('sucess_msg')
+            res.locals.success_msg = req.flash('success_msg')
             res.locals.error_msg = req.flash('error_msg')
             next() 
         })
