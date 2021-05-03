3e3141434cc86aac8838deb4a7904fe4e6ff2fbf
Suelen Almeida <lima.j.diego@gmail.com>
2021-05-03 15:13:10 -0300

    Listando as categorias

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('connect-flash')
+const { schedulingPolicy } = require('cluster')
 
 require('./models/Categoria')
 const Categoria = mongoose.model('categorias')
@@ -63,15 +64,54 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/postagens/:slug', (req, res)=> {
+
+    Postagens.findOne({ slug: req.params.slug }).lean().then((postagens) => {
+        if (postagens) {
+            res.render('postagem/index', { postagens: postagens })
+        } else {
+            req.flash('error_msg', 'Essa postagen não existe!')
+            res.redirect('/')
+        }
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro interno')
+        res.redirect('/')
+    })
+})
+
 app.get('/404', (req, res) => {
     res.send('Erro 404!')
 })
 
 app.get('/categorias', (req, res) => {
     Categoria.find().lean().then((categorias) => {
+        
         res.render('categorias/index', { categorias: categorias })
+    
     }).catch((err) => {
         req.flash('error_msg', 'Houve um erro interno ao listar as categorias')
+        res.redirect('/')
+    })
+})
+
+app.get('/categorias/:slug', (req, res) => {
+    Categoria.findOne({ slug: req.params.slug }).lean().then((categoria) => {
+        if (categoria) {
+
+            Postagens.find({ categoria: categoria._id }).lean().then((postagens) => {
+
+            res.render('categorias/postagens', { postagens: postagens, categoria: categoria })
+
+            }).catch((err) => {
+                req.flash('error_msg', 'Houve um erro a listar os posts!')
+                res.redirect('/')
+            })
+        } else {
+            req.flash('error_msg', 'Está categoria não existe')
+            res.redirect('/')
+        }
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro interno ao carregar a página de categoria')
         res.redirect('/')
     })
 })
